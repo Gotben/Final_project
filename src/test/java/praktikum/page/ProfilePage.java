bc23ccedc85d880503c977aaaf15acf1442b78de
Gotben <gotbanzot@gmail.com>
2026-03-30 22:07:35 +0300

    ПоправУдалил лишние комментарии

--- src/test/java/praktikum/page/ProfilePage.java
+++ src/test/java/praktikum/page/ProfilePage.java
@@ -6,21 +6,18 @@ import org.openqa.selenium.support.FindBy;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.page;
-import static com.codeborne.selenide.Selenide.sleep;
+
 
 @Feature("Профиль пользователя")
 public class ProfilePage {
 
-    //@FindBy(xpath = "//h1[contains(@class, 'h1') and contains(text(), 'Мой профиль')]")
+
     @FindBy(xpath = "//h1[contains(@class, 'h1') and contains(text(), 'Мой профиль')]")
     private SelenideElement profileTitle;
 
-    //@FindBy(xpath = "//div[@class='grid_threeColumns__ldn5D']//div[@class='about']//h2[@class='h2']")
     @FindBy(xpath = "//div[contains(@class, 'about')]//h2[contains(@class, 'h2')]")
-    //@FindBy(xpath = "//div[contains(@class, 'grid_threeColumns')]//div[contains(@class, 'about')]//h2[contains(@class, 'h2')]")
     private SelenideElement titleAd;
 
-    //@FindBy(xpath = "//div[@class='grid_threeColumns__ldn5D']//div[@class='price']//h2[@class='h2']")
     @FindBy(xpath = "//div[contains(@class, 'price')]//h2[contains(@class, 'h2')]")
     private SelenideElement priceAd;
 
